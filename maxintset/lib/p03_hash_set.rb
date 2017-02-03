4ada5161176b558f4b929f0078ef2a314c5b1f0e
Alec Johnson <alecjohnson55@gmail.com>
2017-02-03 13:40:16 -0800

    Add API for HashSet

--- maxintset/lib/p03_hash_set.rb
+++ maxintset/lib/p03_hash_set.rb
@@ -1,7 +1,7 @@
 require_relative 'p02_hashing'
 
 class HashSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -9,18 +9,36 @@ class HashSet
   end
 
   def insert(key)
+    resize! if @count >= num_buckets
+    unless self[key].include?(key)
+      @count += 1
+      self[key] << key
+    end
+    #hash it
+    #mod by length
+    #check for existence
+    #increment count
+    #add
+
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    #check if key was in the bucket
+    #if it was there decrement count
+    #call delete on it
+    @count -= 1 if self[key].include?(key)
+    self[key].delete(key)
   end
 
   private
 
-  def [](num)
+  def [](key)
     # optional but useful; return the bucket corresponding to `num`
+    @store[key.hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +46,12 @@ class HashSet
   end
 
   def resize!
+    new_store = HashSet.new(num_buckets * 2)
+    @store.each do |bucket|
+      bucket.each do |el|
+        new_store.insert(el)
+      end
+    end
+    @store = new_store.store
   end
 end
